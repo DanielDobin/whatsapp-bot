8b6bc367a9c4d27fbd5a1719a85d8546367111f3
DanielDobin <yahides@gmail.com>
2025-02-12 22:37:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MongoDB configuration
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://<db_user>:<db_password>@<db_name>.dxep6.mongodb.net/?retryWrites=true&w=majority&appName=<db_name>';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://<db_user>:<db_password>@<db_name>.dxep6.mongodb.net/?retryWrites=true&w=majority&appName=<project_name>';
 
 async function startBot() {
   try {
